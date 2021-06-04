deccae8076735ddc51c6b10470117fb44719c6e3
Pinioo <jakpin1999@gmail.com>
2021-06-04 20:43:28 +0200

    Prosty, malo praktyczny exception handler dla ApplicationCotroller

--- src/main/kotlin/demo/controllers/ApplicationController.kt
+++ src/main/kotlin/demo/controllers/ApplicationController.kt
@@ -4,12 +4,14 @@ import demo.dto.application.ApplicationLoggedInPayload
 import demo.dto.application.ApplicationNoUserPayload
 import demo.services.ApplicationService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.util.NoSuchElementException
 
 @RestController
 @RequestMapping("/applications")
 class ApplicationController(@Autowired private val applicationService: ApplicationService) {
-
     @PostMapping("/apply/{offerId}/user")
     fun applyForAnOffer(@PathVariable offerId: Int, @RequestBody applicationPayload: ApplicationLoggedInPayload) =
         applicationService.insertApplicationLoggedInUser(offerId, applicationPayload)
@@ -17,4 +19,9 @@ class ApplicationController(@Autowired private val applicationService: Applicati
     @PostMapping("/apply/{offerId}/no-user")
     fun applyForAnOffer(@PathVariable offerId: Int, @RequestBody applicationPayload: ApplicationNoUserPayload) =
         applicationService.insertApplicationWithoutUser(offerId, applicationPayload)
+
+    @ExceptionHandler(NoSuchElementException::class)
+    fun handleNoSuchElementException(): ResponseEntity<Any> {
+        return ResponseEntity("No resource with such id", HttpStatus.NOT_FOUND)
+    }
 }
