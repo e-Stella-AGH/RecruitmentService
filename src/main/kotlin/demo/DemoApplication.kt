0bef7dcf5b3d441a052abd94ada1416590ad5f1c
Pinioo <jakpin1999@gmail.com>
2021-06-07 14:13:59 +0200

    generate-ddl set to true

--- src/main/kotlin/demo/DemoApplication.kt
+++ src/main/kotlin/demo/DemoApplication.kt
@@ -33,7 +33,8 @@ fun getApplicationPropertiesForH2(): String {
         spring.datasource.url=jdbc:h2:file:./myDB
         spring.datasource.username=admin
         spring.datasource.password=admin
-        spring.jpa.hibernate.ddl-auto=create-drop
+        spring.jpa.generate-ddl=true
+        spring.jpa.hibernate.ddl-auto=create
         spring.jpa.show-sql=true
         spring.jpa.database-platform=org.hibernate.dialect.H2Dialect
         spring.datasource.driver-class-name=org.h2.Driver
