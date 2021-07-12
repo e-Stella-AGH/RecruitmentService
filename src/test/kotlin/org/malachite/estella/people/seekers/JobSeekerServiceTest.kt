22bc355c9a7461cca55aae6904af06eb4d3ab0b0
Dawid Dębowski <dawid199960@gmail.com>
2021-07-12 22:28:06 +0200

    mail response in dev/null

--- src/test/kotlin/org/malachite/estella/people/seekers/JobSeekerServiceTest.kt
+++ src/test/kotlin/org/malachite/estella/people/seekers/JobSeekerServiceTest.kt
@@ -11,6 +11,7 @@ import org.malachite.estella.people.users.DummyUserRepository
 import org.malachite.estella.services.JobSeekerService
 import org.malachite.estella.services.MailService
 import org.malachite.estella.services.UserService
+import org.malachite.estella.util.dev.`null`.mailServiceResponse
 import org.malachite.estella.util.jobSeekers
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
@@ -66,70 +67,4 @@ class JobSeekerServiceTest {
 
     private val testUsers = jobSeekers
 
-}
-
-private val mailServiceResponse = object: WebClient.RequestHeadersSpec<Nothing> {
-    override fun accept(vararg acceptableMediaTypes: MediaType?): Nothing {
-        TODO("Not yet implemented")
-    }
-
-    override fun acceptCharset(vararg acceptableCharsets: Charset?): Nothing {
-        TODO("Not yet implemented")
-    }
-
-    override fun cookie(name: String, value: String): Nothing {
-        TODO("Not yet implemented")
-    }
-
-    override fun cookies(cookiesConsumer: Consumer<MultiValueMap<String, String>>): Nothing {
-        TODO("Not yet implemented")
-    }
-
-    override fun ifModifiedSince(ifModifiedSince: ZonedDateTime): Nothing {
-        TODO("Not yet implemented")
-    }
-
-    override fun ifNoneMatch(vararg ifNoneMatches: String?): Nothing {
-        TODO("Not yet implemented")
-    }
-
-    override fun header(headerName: String, vararg headerValues: String?): Nothing {
-        TODO("Not yet implemented")
-    }
-
-    override fun headers(headersConsumer: Consumer<HttpHeaders>): Nothing {
-        TODO("Not yet implemented")
-    }
-
-    override fun attribute(name: String, value: Any): Nothing {
-        TODO("Not yet implemented")
-    }
-
-    override fun attributes(attributesConsumer: Consumer<MutableMap<String, Any>>): Nothing {
-        TODO("Not yet implemented")
-    }
-
-    override fun context(contextModifier: Function<Context, Context>): Nothing {
-        TODO("Not yet implemented")
-    }
-
-    override fun httpRequest(requestConsumer: Consumer<ClientHttpRequest>): Nothing {
-        TODO("Not yet implemented")
-    }
-
-    override fun retrieve(): WebClient.ResponseSpec {
-        TODO("Not yet implemented")
-    }
-
-    override fun <V : Any?> exchangeToMono(responseHandler: Function<ClientResponse, out Mono<V>>): Mono<V> {
-        TODO("Not yet implemented")
-    }
-
-    override fun <V : Any?> exchangeToFlux(responseHandler: Function<ClientResponse, out Flux<V>>): Flux<V> {
-        TODO("Not yet implemented")
-    }
-
-    override fun exchange(): Mono<ClientResponse> {
-        TODO("Not yet implemented")
-    }
 }
