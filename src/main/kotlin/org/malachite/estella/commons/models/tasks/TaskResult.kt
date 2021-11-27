179b1437ecd55c835a08f1d3761674e32a4869e2
Pinioo <jakpin1999@gmail.com>
2021-11-27 16:02:59 +0100

    [fix-taskresult-bug] startTask had been using wrong (data class specific) TaskResult#copy method with named parameters

--- src/main/kotlin/org/malachite/estella/commons/models/tasks/TaskResult.kt
+++ src/main/kotlin/org/malachite/estella/commons/models/tasks/TaskResult.kt
@@ -9,37 +9,18 @@ import javax.persistence.*
 @Entity
 @Table(name = "tasks_results")
 data class TaskResult(
-    @Id @GeneratedValue(strategy = GenerationType.AUTO) val id: Int?,
-    @Lob val results: Blob?, @Lob val code: Clob?,
-    val startTime: Timestamp?, val endTime: Timestamp?,
-    @ManyToOne val task: Task
-) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Int?,
+
+    @Lob val results: Blob?,
+    @Lob val code: Clob?,
+    val startTime: Timestamp?,
+    val endTime: Timestamp?,
+    @ManyToOne val task: Task,
 
     @JsonIgnore
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "taskStage")
-    var taskStage: TaskStage? = null
-
-    constructor(
-        id: Int?,
-        results: Blob?,
-        code: Clob?,
-        startTime: Timestamp?,
-        endTime: Timestamp?,
-        task: Task,
-        taskStage: TaskStage
-    ) : this(id, results, code, startTime, endTime, task) {
-        this.taskStage = taskStage
-    }
-
-    fun copy(
-        id: Int? = this.id,
-        results: Blob? = this.results,
-        code: Clob? = this.code,
-        startTime: Timestamp? = this.startTime,
-        endTime: Timestamp? = this.endTime,
-        task: Task = this.task,
-        taskStage: TaskStage? = this.taskStage
-    ): TaskResult = TaskResult(id, results, code, startTime, endTime, task, taskStage!!)
-
-}
+    val taskStage: TaskStage? = null
+)
